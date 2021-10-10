27b230ff7beb1fd4b5c4427f1491bcfcb745da66
Sergey Kononenko <hackfeed@yandex.ru>
2021-10-11 01:34:14 +0300

    Fix unhandled error in Postgres transaction rollback

--- backend/internal/db/storage/postgres.go
+++ backend/internal/db/storage/postgres.go
@@ -77,7 +77,12 @@ func (pc *PostgresClient) Create(ctx context.Context, table string, fields []str
 	if err != nil {
 		return fmt.Errorf("failed to begin postgres transaction, error is: %s", err)
 	}
-	defer tx.Rollback(ctx)
+	defer func(tx pgx.Tx, ctx context.Context) {
+		err := tx.Rollback(ctx)
+		if err != nil {
+			panic(fmt.Errorf("failed to rollback postgres transaction, error is: %s", err))
+		}
+	}(tx, ctx)
 
 	data := make([][]interface{}, len(invoices))
 	for i := range data {
